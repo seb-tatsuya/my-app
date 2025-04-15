8e30f6bf60ad90cc14b6d5d63cbf7aa323ae4f7f
se_tatsuya_b <b.tatsuya.se@gmail.com>
2025-04-15 23:17:27 +0900

    ContactFormコンポーネントにアクションステートを追加し、バリデーションエラーを表示する機能を実装

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { submitContactForm } from "@/lib/actions/contact";
+import { useActionState } from "react";
 
 export default function ContactForm() {
+  const [state, formAction] = useActionState(submitContactForm, {
+    success: false,
+    errors: {},
+  });
+
   return (
     <div>
-      <form action={submitContactForm}>
+      <form action={formAction}>
         <div className="py-24 text-gray-600">
           <div className="md:w-1/2 bg-white rounded-lg p-8 flex flex-col mx-auto shadow-md">
             {" "}
@@ -19,7 +25,12 @@ export default function ContactForm() {
                 name="name"
                 className="w-full bg-white rounded border border-gray-300
 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none py-1 px-3 leading-8"
-              />{" "}
+              />
+              {state.errors.name && (
+                <p className="text-red-500 text-sm mt-1">
+                  {state.errors.name.join(",")}
+                </p>
+              )}
             </div>
             <div className="mb-4">
               <label htmlFor="email" className="text-sm">
@@ -31,7 +42,12 @@ focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none py-1 px-
                 name="email"
                 className="w-full bg-white rounded border border-gray-300
 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none py-1 px-3 leading-8"
-              />{" "}
+              />
+              {state.errors.email && (
+                <p className="text-red-500 text-sm mt-1">
+                  {state.errors.email.join(",")}
+                </p>
+              )}
             </div>
             <button className="text-white bg-indigo-500 py-2 px-6 hover:bg-indigo-600 rounded text-lg">
               送信
--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -2,17 +2,35 @@
 import { redirect } from "next/navigation";
 import { contactSchema } from "@/validations/contact";
 
+// Action Stateの型定義
+type ActionState = {
+  success: boolean;
+  errors: { name?: string[]; email?: string[] };
+  serverError?: string;
+};
+
 // Server Action（サーバー側で動作）
-export async function submitContactForm(formData: FormData) {
+export async function submitContactForm(
+  prevState: ActionState,
+  formData: FormData
+): Promise<ActionState> {
   const name = formData.get("name");
   const email = formData.get("email");
 
   // バリデーション
-  const validationResult = contactSchema.safeParse({ name, email });
+  const validationResult = contactSchema.safeParse({ name, email }); // safeParseでバリデーションを実行
+  // バリデーションエラーがある場合
   if (!validationResult.success) {
-    const errors = validationResult.error.flatten();
+    // エラーを取得、エラーはzodの型で取得できる
+    const errors = validationResult.error.flatten().fieldErrors;
     console.log("サーバー側でエラー:", errors);
-    return {};
+    return {
+      success: false,
+      errors: {
+        name: errors.name || [],
+        email: errors.email || [],
+      },
+    };
   }
   // DB
 
